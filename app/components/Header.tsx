b8ff5d93fe85d43f3e12df954717535cb50248ac
HeiPavel <112711905HeiPavel@users.noreply.github.com>
2024-09-16 17:24:14 +0300

    Optimized nav links underline on scroll

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,8 +5,10 @@ const linkText = ['home', 'project', 'contact']
 
 export const Header = () => {
   const [isLinkClicked, setIsLinkClicked] = useState(false)
+  const [lastSectionShift, setLastSectionShift] = useState(-1)
   const navRef = useRef<HTMLElement>(null)
   const underlineRef = useRef<HTMLDivElement>(null)
+  console.log(lastSectionShift)
 
   const setUnderline = (link: HTMLAnchorElement) => {
     if (!underlineRef.current) return
@@ -32,46 +34,66 @@ export const Header = () => {
   useEffect(() => {
     if (!navRef.current || window.innerWidth < 640) return
     
-    const links = navRef.current.querySelectorAll('a')
+    const links = [...navRef.current.querySelectorAll('a')]
+    const sections = [...document.querySelectorAll('.section')]
 
-    for (const link of links) {
-      if (link.classList.contains('active')) setUnderline(link)
+    const calculateLastSectionShift = () => {
+      const bodyHeight = document.body.getBoundingClientRect().height
+      const totalSectionsHeight = sections.reduce((sum, section) => sum += section.getBoundingClientRect().height, 0)
+      const lastSectionHeight = sections[sections.length - 1].getBoundingClientRect().height
+      const extraSpace = window.innerHeight - (bodyHeight - totalSectionsHeight) - lastSectionHeight
+      if (lastSectionShift === 0 && extraSpace <= 0) return
+      setLastSectionShift(extraSpace)
     }
 
-    const sections = document.querySelectorAll('.section')
-    const handleScroll = () => {
-      for (const section of sections) {
-        const {top, height} = section.getBoundingClientRect()
-        const multiplier = Math.floor((height / window.innerHeight) * 10) > 7 ? 0.5 : 0.05;
-        const mid = Math.round(window.innerHeight * multiplier)
-
-        if (window.innerHeight - top >= mid) {
-          const activeIndex = Number(section.getAttribute('data-index'))
-
-          links[activeIndex].classList.add('active')
-          setUnderline(links[activeIndex])
-
-          links.forEach((link, index) => {
-            if (link.classList.contains('active') && activeIndex !== index) {
-              link.classList.remove('active')
-            } 
-          })
-        }
+    if (lastSectionShift < 0) {
+      calculateLastSectionShift()
+      for (const link of links) {
+        if (link.classList.contains('active')) setUnderline(link)
       }
+    }
+    
+    const handleScroll = () => {
+      const activeLinkIndex = links.findIndex(link => link.classList.contains('active'))
+      let {top, bottom} = sections[activeLinkIndex].getBoundingClientRect()
+      const {innerHeight} = window
+      const shift = Math.round(innerHeight * 0.5)
+      if (activeLinkIndex === sections.length - 1) top -= lastSectionShift
+      if (activeLinkIndex === sections.length - 2) bottom -= lastSectionShift
+        
+      if (top <= innerHeight - shift && bottom >= innerHeight - shift) return
+
+      let nextIndex = activeLinkIndex
+      top < 0 && bottom < innerHeight - shift ? nextIndex++ : nextIndex--
+
+      links[activeLinkIndex].classList.remove('active')
+      links[nextIndex].classList.add('active')
+      setUnderline(links[nextIndex])
     }
 
     const handleScrollEnd = () => {
       if (isLinkClicked) setIsLinkClicked(false)
     }
 
+    let shiftTimeoutId: NodeJS.Timeout
+
+    const handleLastSectionShiftOnResize = () => {
+      clearTimeout(shiftTimeoutId)
+      shiftTimeoutId = setTimeout(() => {
+        calculateLastSectionShift()
+      }, 200)
+    }
+
     if (!isLinkClicked) window.addEventListener('scroll', handleScroll)
     window.addEventListener('scrollend', handleScrollEnd)
+    window.addEventListener('resize', handleLastSectionShiftOnResize)
 
     return () => {
       window.removeEventListener('scroll', handleScroll)
       window.removeEventListener('scrollend', handleScrollEnd)
+      window.removeEventListener('resize', handleLastSectionShiftOnResize)
     } 
-  }, [isLinkClicked])
+  }, [isLinkClicked, lastSectionShift])
 
   return (
     <header className='fixed z-50 top-0 h-10 tablet:h-12 w-full dark:bg-black-light bg-white'>
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <>
       <ThemeSwitch/>
-      <div data-index='0' className='section h-screen'>Home</div>
-      <div data-index='1' id='project' className='section h-[2000px]'>Project</div>
-      <div data-index='2' id='contact' className='section h-96'>Contact</div>
+      <div className='section h-screen'>Home</div>
+      <div id='project' className='section h-[2000px]'>Project</div>
+      <div id='contact' className='section h-96'>Contact</div>
     </>
   )
 }
