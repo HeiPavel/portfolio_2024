71ad6ed0435f4748d4effa17537e1ef6d4f5fdbd
Pavlo Trofymovych <112711905+HeiPavel@users.noreply.github.com>
2024-09-18 16:34:52 +0300

    Create mobile menu (#2)
    * Handled hover state for ThemeSwitch buttons
    
    * Add mobile nav menu

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <>
       <ThemeSwitch/>
-      <div className='section h-screen'>Home</div>
-      <div id='project' className='section h-[2000px]'>Project</div>
-      <div id='contact' className='section h-96'>Contact</div>
+      <div className='section h-screen pt-12'>Home</div>
+      <div id='project' className='section h-[2000px] pt-12'>Project</div>
+      <div id='contact' className='section h-96 pt-12'>Contact</div>
     </>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from 'tailwindcss'
+import plugin from 'tailwindcss/plugin'
+import { PluginAPI } from 'tailwindcss/types/config'
 
 const config: Config = {
   content: [
@@ -22,15 +24,37 @@ const config: Config = {
     },
     extend: {
       colors: {
-        white: '#FDFDFD',
+        white: {
+          light: '#F3F3F3',
+          default: '#FDFDFD'
+        },
         black: {
           light: '#303031',
           dark: '#0C0C0C'
         },
-        purple: '#CA83FA'
+        purple: '#CA83FA',
+        grey: '#3C3C3D'
       },
+      boxShadow: {
+        'moon': '0 0 10px 5px rgba(56, 3, 92, 0.5)',
+        'sun' : '0 0 10px 5px rgba(255, 167, 59, 0.5)'
+      }
     }
   },
-  darkMode: 'class'
+  darkMode: 'class',
+  plugins: [
+    plugin(function (api: PluginAPI) {
+      const newUtilities = {
+        '.text-shadow-light': {
+          textShadow: '1px 1px 1px rgba(12, 12, 12, 0.3), 1px 2px 1px rgba(12, 12, 12, 0.3), 1px 3px 1px rgba(12, 12, 12, 0.3)'
+        },
+        '.text-shadow-dark': {
+          textShadow: '1px 1px 1px rgba(253, 253, 253, 0.3), 1px 2px 1px rgba(253, 253, 253, 0.3), 1px 3px 1px rgba(253, 253, 253, 0.3)'
+        }
+      }
+
+      api.addUtilities(newUtilities)
+    })
+  ]
 };
 export default config
