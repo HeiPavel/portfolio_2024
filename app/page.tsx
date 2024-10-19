ce50a7e0a5291b4e606df28775e8ef7ccd49d61b
Pavlo Trofymovych <112711905+HeiPavel@users.noreply.github.com>
2024-10-19 21:28:54 +0300

    Handle cdn cache control (#7)
    * Changed favicon
    
    * Updated README
    
    * Switch from unstable cache to time-base revalidation

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { Home } from './components/Home/Home'
 import { ProjectSection } from './components/ProjectSection/ProjectSection'
 import { ContactSection } from './components/ContactSection'
 
+export const revalidate = 43200
+
 export default function Portfolio() {
   return (
     <>
